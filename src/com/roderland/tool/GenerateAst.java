88f434723b054ca4903681046175c06ef97947be
roderland <1434453200@qq.com>
2021-09-30 22:52:19 +0800

    02.Representing Code

---
+++ src/com/roderland/tool/GenerateAst.java
@@ -0,0 +1,96 @@
+package com.roderland.tool;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Roderland
+ * @since 1.0
+ */
+public class GenerateAst {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+            "Binary   : Expr left, Token operator, Expr right",
+            "Grouping : Expr expression",
+            "Literal  : Object value",
+            "Unary    : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types)
+            throws FileNotFoundException, UnsupportedEncodingException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+        writer.println("package com.roderland.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
+        for (String type : types) {
+            String[] item = type.split(":");
+            String className = item[0].trim();
+            String fields = item[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
+        writer.println();
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fields) {
+        writer.println("    static class " + className + " extends " + baseName + " {");
+
+        // Constructor.
+        writer.println("        " + className + "(" + fields + ") {");
+        String[] fieldArray = fields.split(",");
+        for (String field : fieldArray) {
+            String name = field.trim().split(" ")[1];
+            writer.println("            this." + name + " = " + name + ";");
+        }
+        writer.println("        }");
+
+        writer.println();
+
+        // Visitor pattern
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
+        // Fields.
+        for (String field : fieldArray) {
+            writer.println("        final " + field.trim() + ";");
+        }
+
+        writer.println("    }");
+        writer.println();
+    }
+}
